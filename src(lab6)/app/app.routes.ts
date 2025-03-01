18e2e5d3717a4ecf4e65fa9975a8d111fb017010
AbylayBeisen <156783263+AbylayBeisen@users.noreply.github.com>
2025-03-01 15:27:26 +0500

    Lab6
    Lab6

---
+++ src(lab6)/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AlbumComponent } from './albums/albums.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { PhotosComponent } from './photos/photos.component';
+import { AppComponent } from './app.component';  
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'albums', component: AlbumComponent },
+    { path: 'albums/:id', component: AlbumDetailComponent },
+    { path: 'albums/:id/photos', component: PhotosComponent },
+    { path: '**', redirectTo: '' } 
+];
